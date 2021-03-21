8e5e7d11d21369b2ffbbdd29421797b62c5527b9
durasel74 <diman174cheb@mail.ru>
2021-03-21 22:45:49 +0500

    Number list element changing

--- Algorithms/Algorithm/BinarySearch/BSViewModel.cs
+++ Algorithms/Algorithm/BinarySearch/BSViewModel.cs
@@ -4,8 +4,21 @@ namespace Algorithms.Algorithm.BinarySearch
 {
 	public class BSViewModel : ViewModel
 	{
+		private int viewElementStyle;	
+
+		public int ViewElementStyle
+		{ 
+			get { return viewElementStyle; }
+			set
+			{
+				viewElementStyle = value;
+				OnPropertyChanged("ViewElementStyle");
+			}
+		}
+
         public BSViewModel()
 		{
+			viewElementStyle = 1;
 			Algorithm = new BinarySearch();
 		}
 	}

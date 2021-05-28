e0c3ea46981df5c1a56aae6e297ee6183186be49
durasel74 <diman174cheb@mail.ru>
2021-05-28 17:45:37 +0500

    Tatu release

--- Algorithms/Algorithm/ViewModel.cs
+++ Algorithms/Algorithm/ViewModel.cs
@@ -2,11 +2,70 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
+
+
+
+using System.Windows;
+
+
+
+
 namespace Algorithms.Algorithm
 {
     public class ViewModel : INotifyPropertyChanged
-	{
+    {
         private AlgorithmsProcessor algorithm;
+
+        #region Временное
+        private bool showButtonList = false;
+        public bool ShowButtonList
+        {
+            get { return showButtonList; }
+            set
+            {
+                showButtonList = value;
+                OnPropertyChanged("ShowButtonList");
+            }
+        }
+        private ButtonCommand authorInfo;
+        public ButtonCommand AuthorInfo
+        {
+            get
+            {
+                return authorInfo ??
+                    (authorInfo = new ButtonCommand(obj =>
+                    { 
+                        MessageBox.Show("Автор: Коротовский Дмитрий, " +
+                            "курсант 331 группы Троицкого Авиационного колледжа."); 
+                    }));
+            }
+        }
+        private ButtonCommand programInfo;
+        public ButtonCommand ProgramInfo
+        {
+            get
+            {
+                return programInfo ??
+                    (programInfo = new ButtonCommand(obj =>
+                    {
+                        MessageBox.Show("Программа для визуализации " +
+                            "популярных алгоритмов.");
+                    }));
+            }
+        }
+        private ButtonCommand exitProgram;
+        public ButtonCommand ExitProgram
+        {
+            get
+            {
+                return exitProgram ??
+                    (exitProgram = new ButtonCommand(obj =>
+                    {
+                        Application.Current.Shutdown();
+                    }));
+            }
+        }
+        #endregion
 
         // Команда старта алгоритма
         private ButtonCommand startCommnad;
